1367dda637dd7f436554964938472d9ee312af93
Pushar Singhal <pusinghal010@gmail.com>
2025-10-04 13:56:40 +0530

    added crud operation using class view

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('info/',views.infoView),
-    path('info/<int:pk>/',views.infoDetailView),
+    path('info/',views.InfoList.as_view()),
+    path('info/<int:pk>/',views.InfoDetail.as_view()),
 ]
--- notes/api/views.py
+++ notes/api/views.py
@@ -1,39 +1,42 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse,Http404
 from info.models import Info
 from .serializers import InfoSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-# Create your views here.
-@api_view(['GET','POST'])
-def infoView(request):
-    if request.method=='GET':
+class InfoList(APIView):
+
+    def get(self,request):
         info=Info.objects.all()
         serializer=InfoSerializer(info, many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
-
-    elif request.method=='POST':
+    
+    def post(self,request):
         serializer=InfoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET','PUT','DELETE'])
-def infoDetailView(request,pk):
-    try:
-        info=Info.objects.get(pk=pk)
-    except Info.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method=='GET':
+
+class InfoDetail(APIView):
+
+    def get_object(self,pk):
+        try:
+            return Info.objects.get(pk=pk)
+        except Info.DoesNotExist:
+            raise Http404
+        
+    def get(self,request,pk):
+        info=self.get_object(pk)
         serializer=InfoSerializer(info)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
-    elif request.method=='PUT':
+    def put(self,request,pk):
+        info=self.get_object(pk)
         serializer=InfoSerializer(info,data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -41,8 +44,7 @@ def infoDetailView(request,pk):
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
-    elif request.method=='DELETE':
+    def delete(self,request,pk):
+        info=self.get_object(pk)
         info.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
